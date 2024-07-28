7594385d948a0eff67df8fef6e1fcb3f3108292b
Sims <njabulos2001@gmail.com>
2024-07-28 16:22:17 +0200

    DEV: Update ActorsService to use BaseRepository.

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using eTickets.Data.Base;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace eTickets.Models
 {
-    public class Actor
+    public class Actor : IEntityBase
     {
         [Key]
         public int Id { get; set; }
